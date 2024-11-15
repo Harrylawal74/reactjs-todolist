f37fd11b59ac039350148001fb52fcf89f6fef26
Harry Lawal <harrylawal74@gmail.com>
2024-11-15 23:04:35 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import TodoInput from "./components/Todoinput"
+import TodoList from "./components/TodoList"
+
+function App() {
+  
+
+  return (
+    <>
+    <main>
+      <TodoInput/>
+      <TodoList/>
+    </main>
+
+    </>
+  )
+}
+
+export default App
