8fad42686fa80a21710d37e5d00c8f7cb77269a7
Harry Lawal <harrylawal74@gmail.com>
2024-11-19 17:04:35 +0000

    update Todo list stays the same when page is refreshed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 
@@ -12,11 +12,24 @@ function App() {
   //the string is a useState becuase it is dynamic and can change value
   const [todoValue, setTodoValue] = useState('')
 
+  //creates an item in localStorage with the key 'todos' (this is the todo list)
+  //takes newList as a param
+  //replaces the list stored in localStorage with the newlist
+  //this function should be called each time the list is updated (deleted from or added to. Does not need do be done for editing a list item becuase the add function is used within the edit function)
+  function persistData(newList) {
+    localStorage.setItem('todos', JSON.stringify({ todos:
+      newList
+    }))
+  }
+
   ///takes newTodo as a parameter then creates a new array called newTodoList which concatinates the new item to the old list
   //the ...todos notation speads out the values in the array
   ///setTodos function takes newTodoList as a parameter and updates the todos array
   function handleAddTodos(newTodo) {
     const newTodoList = [...todos, newTodo]
+
+    //persistData() function being called to update the todos list in localStorage
+    persistData(newTodoList)
     setTodos(newTodoList)
   }
 
@@ -26,6 +39,9 @@ function App() {
     const newTodoList = todos.filter((todo, todoIndex) => {
       return todoIndex !== index 
     })
+
+    //persistData() function being called to update the todos list in localStorage
+    persistData(newTodoList)
     setTodos(newTodoList)
   }
 
@@ -38,6 +54,26 @@ function App() {
     setTodoValue(valueToBeEdited)
     handleDeleteTodo(index)
   }
+
+
+useEffect(() => {
+  //checks whether local storage exists, if it doesn't then returns out
+  if (!localStorage) {
+    return 
+  }
+
+  //checks whether todos exists in local storage, if it doesn't then returns out
+  let localTodos = localStorage.getItem('todos')
+  if (!localTodos){
+    return
+  }
+
+  //if todos exists in local storage then the value is pasrsed to the setTodos() function
+  localTodos = JSON.parse(localTodos).todos
+  setTodos(localTodos)
+  
+  //square brackets are empty therefore the 'useEffect' react hook is triggered when the page is refreshed 
+}, [])
 
   return (
     ///give other components access to variables defined in Apps.jsx
